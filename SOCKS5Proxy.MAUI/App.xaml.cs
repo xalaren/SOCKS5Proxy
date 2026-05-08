0161cdd7d3f05abe876cebfe30985777aa58646e
xalaren <imanov.03.valera@gmail.com>
2026-05-08 16:31:40 +0500

    Add MAUI project

---
+++ SOCKS5Proxy.MAUI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SOCKS5Proxy.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ SOCKS5Proxy.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SOCKS5Proxy.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
